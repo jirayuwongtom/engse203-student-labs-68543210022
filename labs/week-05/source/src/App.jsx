209c18b1a98bf41ad8bf9531c27f75db4c4abcea
jirayuwongtom <lovelava600@gmail.com>
2026-08-13 20:04:20 +0700

    feat: Compile all 5 routes CP02

--- labs/week-05/source/src/App.jsx
+++ labs/week-05/source/src/App.jsx
@@ -1,13 +1,24 @@
+import { Route, Routes } from 'react-router-dom';
+import AboutPage from './pages/AboutPage.jsx';
 import AppLayout from './pages/AppLayout.jsx';
 import DashboardPage from './pages/DashboardPage.jsx';
+import NewRequestPage from './pages/NewRequestPage.jsx';
+import NotFoundPage from './pages/NotFoundPage.jsx';
+import RequestDetailPage from './pages/RequestDetailPage.jsx';
+
 
 function App() {
   return (
-    <AppLayout>
-      <DashboardPage />
-    </AppLayout>
+    <Routes>
+     <Route element={<AppLayout />}>
+        <Route index element={<DashboardPage />} />
+        <Route element={<NewRequestPage />} path="requests/new" />
+        <Route element={<RequestDetailPage />} path="requests/:requestId" />
+        <Route element={<AboutPage />} path="about" />
+        <Route element={<NotFoundPage />} path="*" />
+      </Route>
+    </Routes>
   );
-  // TODO 5A-CP02: เปลี่ยนเป็น <Routes> ที่มี AppLayout เป็นกรอบ
 }
 
 export default App;
