f31413390ca3bf4e04f5013b000911f22880b5ca
jirayuwongtom <lovelava600@gmail.com>
2026-09-10 17:38:38 +0700

    LAB06 in-class: CRUD API

--- labs/week-06/source/src/app.js
+++ labs/week-06/source/src/app.js
@@ -19,6 +19,6 @@ export function createApp() {
    * TODO W06-A4 (🏠 CP07) · ปิดท้ายด้วย notFound แล้วตามด้วย errorHandler
    * ⚠ สองตัวนี้ต้องอยู่ท้ายสุด หลัง route ทั้งหมด
    */
-
+  
   return app;
 }
